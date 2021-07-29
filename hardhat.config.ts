ac43d8305f01411ae6e7f3d915ccc04121abfd4a
Team LevX <team@levx.io>
2021-07-29 19:26:25 +0900

    Hardhat-ready

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,27 +1,80 @@
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "dotenv/config";
+import "hardhat-deploy";
 import "hardhat-typechain";
 import { HardhatUserConfig } from "hardhat/types";
 
+const accounts = [
+  `0x${
+      process.env.PRIVATE_KEY ||
+      "0000000000000000000000000000000000000000000000000000000000000000"
+  }`,
+];
+
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.6.12",
+    compilers: [
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    alice: {
+      default: 1,
+    },
+    bob: {
+      default: 2,
+    },
+    carol: {
+      default: 3,
+    },
   },
   networks: {
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.ADMIN || ''],
-      chainId: 4,
+    localhost: {
+      live: false,
+      saveDeployments: true,
+      tags: ["local"],
+    },
+    hardhat: {
+      live: false,
+      saveDeployments: true,
+      tags: ["test", "local"],
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 1,
+      tags: ["production"],
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 3,
+      tags: ["staging"],
     },
     kovan: {
       url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.ADMIN || ''],
+      accounts,
       chainId: 42,
+      tags: ["staging"],
     },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  mocha: {
+    timeout: 50000,
   },
 };
 
